118fa207904f7565e8a6a98ac0c148faf1a2b4ce
iTheEK <edgar16xdd@gmail.com>
2025-12-09 01:28:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Lista de jugadores
 // Lista de jugadores
 const players = [
+        {
+    nick: "RickyEdits",
+    points: 9,
+    skin: "https://render.crafty.gg/3d/bust/RickyEdits",
+    tiers: { Uhc: "LT3", NethPot: "LT4" }
+  },
       {
     nick: "Kevin__",
     points: 1,
@@ -229,6 +235,7 @@ document.getElementById("search").addEventListener("input", (e) => {
 
 // Render inicial
 renderPlayers(rankedPlayers);
+
 
 
 
